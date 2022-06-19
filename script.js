b5566e01300c8d6cb486c2eb91f2b64f339ca4ba
Sercan Lülleci <srcn_llc_@hotmail.com>
2022-06-19 11:45:19 +0300

    Link güncellemesi yapıldı

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const indirözgecmis=() =>{
     location.href="https://drive.google.com/file/d/1GXQKdyl_aLkHYpnHAmWgU724X0hxfsPR/view?usp=sharing";
 }
 const indirtranskript=() =>{
-    location.href="https://drive.google.com/file/d/17V2dsdN32D5QlIciToNCYHLiLgxC6o3V/view?usp=sharing";
+    location.href="https://drive.google.com/file/d/1p0zzQNxjktipFqiKoupkJiuHjZ7Z5WHu/view?usp=sharing";
 }
 const indirjs=() =>{
     location.href="https://drive.google.com/file/d/1RLgPP0Wst9h4e3d5aQSCeDcLK8kZSmSG/view?usp=sharing";
