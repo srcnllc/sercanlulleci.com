e92bd960c8d0d0f53bc170d39a792b1b55fd74c8
Sercan LÜLLECİ <56175274+srcnllc@users.noreply.github.com>
2022-11-23 21:31:59 +0300

    proje listesi güncellendi

--- src/db.js
+++ src/db.js
@@ -3,153 +3,160 @@ const dbArray = {
     "project": [
         {
             "ProjectId": 1,
+            "ProjectName": "bi-doner_e-siparis",
+            "img": "https://s1.gifyu.com/images/ekrangoruntusu.jpg",
+            "link": "https://www.youtube.com/watch?v=NYtkeLXgAUI&t=30s",
+            "gitHub": "https://github.com/srcnllc/bi-doner_e-siparis",
+        },
+        {
+            "ProjectId": 2,
             "ProjectName": "Car-Accessories-Ecommerce-Web-Template",
             "img": "https://s4.gifyu.com/images/Car-Accessories-Ecommerce-Web-Template-Free-PSD.jpg",
             "link": "https://www.youtube.com/watch?v=uiJfvCJW66E&t=18s",
             "gitHub": "https://github.com/srcnllc/Car-Accessories-Ecommerce-Web-Template",
         },
         {
-            "ProjectId": 2,
+            "ProjectId": 3,
             "ProjectName": "AuthFirebase",
             "img": "https://s4.gifyu.com/images/login-register.jpg",
             "link": "https://www.youtube.com/watch?v=Cxsc0MRGxW0&t=30s",
             "gitHub": "https://github.com/srcnllc/AuthFirebase",
         },
         {
-            "ProjectId": 3,
+            "ProjectId": 4,
             "ProjectName": "rest countries api",
             "img": "https://s5.gifyu.com/images/rest-countries-api.jpg",
             "link": "https://www.youtube.com/watch?v=MITSJap0YbA&t=1s",
             "gitHub": "https://github.com/srcnllc/rest-countries-api",
         },
         {
-            "ProjectId": 4,
+            "ProjectId": 5,
             "ProjectName": "advice generator app main",
             "img": "https://s4.gifyu.com/images/advice-generator.jpg",
             "link": "https://www.youtube.com/watch?v=5ckC17NYPVE&t=52s",
             "gitHub": "https://github.com/srcnllc/advice-generator-app-main"
         },
         {
-            "ProjectId": 5,
+            "ProjectId": 6,
             "ProjectName": "pokeApi",
             "img": "https://s4.gifyu.com/images/pokeApi.jpg",
             "link": "https://www.youtube.com/watch?v=mSMCOad1uGs&t=3s",
             "gitHub": "https://github.com/srcnllc/pokeApi"
         },
         {
-            "ProjectId": 6,
+            "ProjectId": 7,
             "ProjectName": "React rondom Api çalışaması",
             "img": "https://s4.gifyu.com/images/React-rondom-Api.jpg",
             "link": "https://www.youtube.com/watch?v=lf0GKFMUzhs&t=8s",
             "gitHub": "https://github.com/srcnllc/Api-work"
         },
         {
-            "ProjectId": 7,
+            "ProjectId": 8,
             "ProjectName": "daily weather",
             "img": "https://s4.gifyu.com/images/daily-weather.jpg",
             "link": "https://www.youtube.com/watch?v=Umkgvnl62j0&t=98ss",
             "gitHub": "https://github.com/srcnllc/daily_weather.github.io"
         },
         {
-            "ProjectId": 8,
+            "ProjectId": 9,
             "ProjectName": "Covid-19 data",
             "img": "https://s4.gifyu.com/images/Covid-19_data.jpg",
             "link": "https://www.youtube.com/watch?v=Vh8nXmYQZHU",
             "gitHub": "https://github.com/srcnllc/covid_19_Analiz"
         },
         {
-            "ProjectId": 9,
+            "ProjectId": 10,
             "ProjectName": "localstorage-helpfulfrontend",
             "img": "https://s4.gifyu.com/images/helpfulfrontend.jpg",
             "link": "https://www.youtube.com/watch?v=aG0PYy2y6II",
             "gitHub": "https://github.com/srcnllc/localstorage-helpfulfrontend"
         },
         {
-            "ProjectId": 10,
+            "ProjectId": 11,
             "ProjectName": "Alışveriş sepet",
             "img": "https://s4.gifyu.com/images/alisveris-sepet.png",
             "link": "https://www.youtube.com/watch?v=dVKC5zF5D7M&t=2s",
             "gitHub": "https://github.com/srcnllc/Alisveris-Sepeti"
         },
         {
-            "ProjectId": 11,
+            "ProjectId": 12,
             "ProjectName": "To do Uygulaması",
             "img": "https://s5.gifyu.com/images/todo.jpg",
             "link": "https://www.youtube.com/watch?v=JbL3nimPkb8",
             "gitHub": "https://github.com/srcnllc/todo"
         },
         {
-            "ProjectId": 12,
+            "ProjectId": 13,
             "ProjectName": "qr code component main",
             "img": "https://s4.gifyu.com/images/qr-code.jpg",
             "link": "https://www.youtube.com/watch?v=SDFi6gVcxvA",
             "gitHub": "https://github.com/srcnllc/qr-code-component-main"
         },
         {
-            "ProjectId": 13,
+            "ProjectId": 14,
             "ProjectName": "preview card component main",
             "img": "https://s5.gifyu.com/images/preview-card.jpg",
             "link": "https://www.youtube.com/watch?v=RCuxQ26nVkI",
             "gitHub": "https://github.com/srcnllc/preview-card-component-main"
         },
         {
-            "ProjectId": 14,
+            "ProjectId": 15,
             "ProjectName": "nft preview card component main",
             "img": "https://s4.gifyu.com/images/nft-preview-card.jpg",
             "link": "https://www.youtube.com/watch?v=8QrHdfp_Nkg&t=2s",
             "gitHub": "https://github.com/srcnllc/nft-preview-card-component-main-"
         },
         {
-            "ProjectId": 15,
+            "ProjectId": 16,
             "ProjectName": "Sayaç",
             "img": "https://s5.gifyu.com/images/basit-sayac.jpg",
             "link": "https://www.youtube.com/watch?v=fbN8uCFRw2E",
             "gitHub": "https://github.com/srcnllc/React_basit_sayac"
         },
         {
-            "ProjectId": 16,
+            "ProjectId": 17,
             "ProjectName": "Frontend developer loading",
             "img": "https://s4.gifyu.com/images/loading.jpg",
             "link": "https://www.youtube.com/watch?v=UEzsVcYIPUw",
             "gitHub": "https://github.com/srcnllc/Loading_Animation"
         },
         {
-            "ProjectId": 17,
+            "ProjectId": 18,
             "ProjectName": "React-Kullanıcı Giriş Formu",
             "img": "https://s5.gifyu.com/images/giris-form.jpg",
             "link": "https://www.youtube.com/watch?v=mff2eUWaipc",
             "gitHub": "https://github.com/srcnllc/Kullanici_giris_formu"
         },
         {
-            "ProjectId": 18,
+            "ProjectId": 19,
             "ProjectName": "Devrek Tanıtım",
             "img": "https://s5.gifyu.com/images/devrek.jpg",
             "link": "https://www.youtube.com/watch?v=zMpdd2jvdIM",
             "gitHub": "https://github.com/srcnllc/DevrekTanitimSitesi"
         },
         {
-            "ProjectId": 19,
+            "ProjectId": 20,
             "ProjectName": "Katherine Johnson wiki",
             "img": "https://s5.gifyu.com/images/katherine.jpg",
             "link": "https://www.youtube.com/watch?v=hVSWbi6oEyo",
             "gitHub": "https://github.com/srcnllc/Katherine_Johnson-wikipedia"
         },
         {
-            "ProjectId": 20,
+            "ProjectId": 21,
             "ProjectName": "instagram clone",
             "img": "https://s5.gifyu.com/images/instagramclon.jpg",
             "link": "https://www.youtube.com/watch?v=5bCjYBX4AqE",
             "gitHub": "https://github.com/srcnllc/Instagram_klon"
         },
         {
-            "ProjectId": 21,
+            "ProjectId": 22,
             "ProjectName": "Order automatioOtomasyon",
             "img": "https://s5.gifyu.com/images/siparis.jpg",
             "link": "https://www.youtube.com/watch?v=fHShZsm5dRw&t=5s",
             "gitHub": "https://github.com/srcnllc/Order-automation"
         },
         {
-            "ProjectId": 22,
+            "ProjectId": 23,
             "ProjectName": "Javascript Algoritma Örnekleri",
             "img": "https://s5.gifyu.com/images/js.png",
             "link": "https://github.com/srcnllc/Javascript_Algoritma",
